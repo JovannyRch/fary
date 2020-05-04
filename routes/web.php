9467025c199c79a66067a700f3662e8e6726820f
jovannyrch <jovannyrch@gmail.com>
2020-05-04 09:13:31 -0500

    Cargando rutas de la WEB

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'WebController@index')->name('index');
+Route::get('/upload/image', 'WebController@uploadImage')->name('uploadImage');
+
+//POST
+
+
+Route::get('/posts/create', 'WebController@createPost')->name('createpost');
+Route::post('/posts/save', 'api\PostsController@create')->name('post.save');
+Route::get('/post/{id}', 'WebController@showPost')->name('showPost');
+
+
+//CARS
+Route::get('/cars', 'WebController@index')->name('cars');
+Route::get('/cars/create', 'WebController@createCarPost')->name('newcar');
+Route::post('/cars/save', 'api\CarsController@store')->name('cars.save');
+
+
+//Admin
+Route::get('/dashboard','AdminController@dashboard')->middleware('admin')->name('admin-index');
+Route::resource('comment', 'CommentsController')->middleware("admin");
+Route::delete('commentCar/{id}', 'CommentsController@destroyCarComment')->middleware("admin")->name('comment.car.destroy');
+Route::resource('commentcar', 'CommentsController')->middleware("admin");
+Route::resource('admin/post', 'dashboard\PostController')->middleware("admin");
+Route::resource('admin/user', 'dashboard\UserController')->middleware('admin');
+Route::resource('admin/car', 'dashboard\CarController')->middleware('admin');
+Route::resource('admin/ads', 'dashboard\AdsController')->middleware('admin');
+Route::resource('admin/negocios', 'dashboard\NegociosController')->middleware('admin');
+Route::resource('admin/tipos', 'dashboard\TiposController')->middleware('admin');
+
+
+//Informativos
+Route::get('/mision', 'WebController@index')->name('mision');
+Route::get('/vision', 'WebController@index')->name('vision');
+Route::get('/aviso-privacidad', 'WebController@index')->name('aviso-privacidad');
+Route::get('/ayuda', 'WebController@index')->name('ayuda');
+Route::get('/zona-publicitaria', 'WebController@index')->name('zona');
+Route::get('/zona-publicitaria/{id}', 'WebController@index')->name('zona-show');

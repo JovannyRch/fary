81416c568916dd6f870b9fa5712862c862254303
jovannyrch <jovannyrch@gmail.com>
2020-05-04 07:48:42 -0500

    Añadí las rutas de la API

--- routes/api.php
+++ routes/api.php
@@ -10,10 +10,41 @@ use Illuminate\Support\Facades\Route;
 |
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| is assigned the "API" middleware group. Enjoy building your API!
 |
-*/
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:API')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+
+
+
+ //Posts
+ Route::post('posts/', 'API\PostsController@create');
+ Route::get('posts/', 'API\PostsController@index');
+ Route::get('posts/{id}', 'API\PostsController@show');
+ Route::delete('posts/{id}', 'API\PostsController@destroy');
+ 
+ //Comments
+ Route::post('comments','API\CommentsController@store' ); 
+ Route::delete('comments/{comment_id}','API\CommentsController@destroy' ); 
+ Route::get('posts/{post_id}/comments', 'API\CommentsController@getComments');
+ 
+ 
+ //Cars
+ Route::get('cars/','API\CarsController@index'); 
+ Route::post('cars/','API\CarsController@store'); 
+ Route::delete('cars/{id}','API\CarsController@destroy'); 
+ 
+ 
+ //Comentarios de post de autos usados
+ Route::post('car_comments','API\CommentCarPostsController@store' ); 
+ Route::delete('car_comments/{comment_id}','API\CommentCarPostsController@destroy' ); 
+ Route::get('cars/{car_id}/comments', 'API\CommentCarPostsController@getComments');
+ 
+ 
+ // Traer toda la publicidad de negocios
+ Route::get('ads','API\AdsController@index' ); 
+ Route::get('tipos','API\TiposController@index' ); 
+ Route::get('tipos/{id}','API\TiposController@negocios' ); 

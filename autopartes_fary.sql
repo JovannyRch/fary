0f381798d1bd88c5d9bdb86ab977e157290ab65b
jovannyrch <jovannyrch@gmail.com>
2020-05-16 18:43:32 -0500

    Updating database

--- autopartes_fary.sql
+++ autopartes_fary.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 01-05-2020 a las 20:01:20
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.1
+-- Servidor: localhost
+-- Tiempo de generación: 17-05-2020 a las 01:42:36
+-- Versión del servidor: 10.1.36-MariaDB
+-- Versión de PHP: 5.6.38
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,38 +25,13 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ads`
---
-
-CREATE TABLE `ads` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `url` text NOT NULL,
-  `negocio_id` bigint(20) UNSIGNED DEFAULT NULL,
-  `tiempo` tinyint(3) UNSIGNED DEFAULT 7,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `ads`
---
-
-INSERT INTO `ads` (`id`, `url`, `negocio_id`, `tiempo`, `created_at`, `updated_at`) VALUES
-(4, '/images/1588258838.jpeg', 1, 10, '2020-04-30 20:00:38', '2020-04-30 20:00:38'),
-(5, '/images/1588258975.mp4', 1, 7, '2020-04-30 20:02:55', '2020-04-30 20:02:55'),
-(6, '/images/1588259535.jpeg', NULL, 7, '2020-04-30 20:12:15', '2020-04-30 20:12:15'),
-(7, '/images/1588259547.jpeg', NULL, 7, '2020-04-30 20:12:27', '2020-04-30 20:12:27');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `cars`
 --
 
 CREATE TABLE `cars` (
   `id` bigint(20) UNSIGNED NOT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `content` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -66,7 +41,10 @@ CREATE TABLE `cars` (
 --
 
 INSERT INTO `cars` (`id`, `user_id`, `created_at`, `updated_at`, `content`) VALUES
-(12, 1, '2020-04-14 09:29:24', '2020-04-14 09:29:24', 'VENTO COMFORTLINE');
+(1, 6, '2020-05-16 08:48:28', '2020-05-16 08:48:28', 'Math Rush'),
+(2, 6, '2020-05-16 08:56:10', '2020-05-16 08:56:10', 'asd'),
+(3, 6, '2020-05-16 08:57:46', '2020-05-16 08:57:46', 'asdasdasd'),
+(4, 7, '2020-05-17 03:54:55', '2020-05-17 03:54:55', '2I GRAND TOURING');
 
 -- --------------------------------------------------------
 
@@ -83,6 +61,15 @@ CREATE TABLE `comments` (
   `post_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Volcado de datos para la tabla `comments`
+--
+
+INSERT INTO `comments` (`id`, `content`, `created_at`, `updated_at`, `user_id`, `post_id`) VALUES
+(2, 'Ejemplo de comentario', '2020-05-16 02:13:48', '2020-05-16 02:13:48', 6, 19),
+(4, 'Un comentario', '2020-05-17 03:26:18', '2020-05-17 03:26:18', 7, 25),
+(5, 'asd', '2020-05-17 04:00:57', '2020-05-17 04:00:57', 7, 25);
+
 -- --------------------------------------------------------
 
 --
@@ -94,7 +81,7 @@ CREATE TABLE `comments_car_posts` (
   `user_id` bigint(20) UNSIGNED NOT NULL,
   `car_post_id` bigint(20) UNSIGNED NOT NULL,
   `content` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -103,7 +90,11 @@ CREATE TABLE `comments_car_posts` (
 --
 
 INSERT INTO `comments_car_posts` (`id`, `user_id`, `car_post_id`, `content`, `created_at`, `updated_at`) VALUES
-(3, 1, 12, 'Transmisión: Std Llaves: SI Refacturación: Si a su nombre, Observaciones:  Arrancando, caminando, Fácil reparación!  Súper Margen!!  TEL 5510434007 / 5526210241', '2020-04-14 09:29:37', '2020-04-14 09:29:37');
+(1, 7, 3, 'Hola', '2020-05-17 00:23:22', '2020-05-17 00:23:22'),
+(2, 7, 3, 'hola a todos', '2020-05-17 00:25:24', '2020-05-17 00:25:24'),
+(3, 7, 3, 'Otro comentario', '2020-05-17 00:47:40', '2020-05-17 00:47:40'),
+(4, 7, 2, 'Un comentario', '2020-05-17 01:36:26', '2020-05-17 01:36:26'),
+(5, 7, 4, 'Un comentario', '2020-05-17 04:18:52', '2020-05-17 04:18:52');
 
 -- --------------------------------------------------------
 
@@ -117,7 +108,7 @@ CREATE TABLE `failed_jobs` (
   `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -128,8 +119,8 @@ CREATE TABLE `failed_jobs` (
 
 CREATE TABLE `imgs_cars` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `url` text DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `url` text,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `car_id` bigint(20) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -139,9 +130,10 @@ CREATE TABLE `imgs_cars` (
 --
 
 INSERT INTO `imgs_cars` (`id`, `url`, `created_at`, `updated_at`, `car_id`) VALUES
-(21, '/images/15868385640.jpeg', '2020-04-14 09:29:24', '2020-04-14 09:29:24', 12),
-(22, '/images/15868385641.jpeg', '2020-04-14 09:29:24', '2020-04-14 09:29:24', 12),
-(23, '/images/15868385642.jpeg', '2020-04-14 09:29:24', '2020-04-14 09:29:24', 12);
+(1, '/images/15896009081.png', '2020-05-16 08:48:28', '2020-05-16 08:48:28', 1),
+(2, '/images/15896013701.png', '2020-05-16 08:56:10', '2020-05-16 08:56:10', 2),
+(3, '/images/15896014661.png', '2020-05-16 08:57:46', '2020-05-16 08:57:46', 3),
+(4, '/images/15896696951.png', '2020-05-17 03:54:55', '2020-05-17 03:54:55', 4);
 
 -- --------------------------------------------------------
 
@@ -218,12 +210,8 @@ CREATE TABLE `password_resets` (
 
 CREATE TABLE `posts` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `piece` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `model` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `brand` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `national` tinyint(1) NOT NULL DEFAULT 1,
   `img` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `details` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   `user_id` bigint(20) UNSIGNED NOT NULL
@@ -233,74 +221,13 @@ CREATE TABLE `posts` (
 -- Volcado de datos para la tabla `posts`
 --
 
-INSERT INTO `posts` (`id`, `piece`, `model`, `brand`, `national`, `img`, `details`, `created_at`, `updated_at`, `user_id`) VALUES
-(4, 'motor 4', '2017 3', '123 4', 1, '', 'asdasd123', '2020-04-09 07:58:59', '2020-04-09 07:58:59', 1),
-(5, 'motor 5', '2017 3', '123 4', 1, NULL, 'asdasd123', '2020-04-09 07:59:01', '2020-04-09 07:59:01', 1),
-(6, 'motor 6', '2017 3', '123 4', 1, NULL, 'asdasd123', '2020-04-09 07:59:04', '2020-04-09 07:59:04', 1),
-(7, 'Llantas', '123', '123', 1, NULL, '123', '2020-04-09 08:29:22', '2020-04-09 08:29:22', 1),
-(8, 'Juguito de arándano', '312', '123', 0, '/images/1586466648.png', '123', '2020-04-10 02:10:48', '2020-04-10 02:10:48', 1),
-(9, 'Juguito de arándano', '312', '123', 0, '/images/1586466886.png', '123', '2020-04-10 02:14:46', '2020-04-10 02:14:46', 1),
-(10, 'Juguito de arándano', '312', '123', 0, '/images/1586466936.png', '123', '2020-04-10 02:15:36', '2020-04-10 02:15:36', 1),
-(13, 'Juguito de arándano', 'asd', 'as', 1, NULL, 'assa', '2020-04-10 04:01:13', '2020-04-10 04:01:13', 1),
-(14, 'Xiaomi', 'asd', 'asd', 1, '/images/1586473288.jpeg', 'asd', '2020-04-10 04:01:28', '2020-04-10 04:01:28', 1),
-(17, 'Juguito de arándano', 'asd', 'asd', 1, '/images/1586473629.png', 'asd', '2020-04-10 04:07:09', '2020-04-10 04:07:09', 1),
-(18, 'Puerta de camioneta', '2014', 'FORD F150', 1, NULL, 'Puerta del lado izquierdo', '2020-04-10 09:58:21', '2020-04-10 09:58:21', 1);
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipos`
---
-
-CREATE TABLE `tipos` (
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tipos`
---
-
-INSERT INTO `tipos` (`created_at`, `updated_at`, `id`, `name`) VALUES
-('2020-04-28 10:25:42', '2020-04-28 10:25:42', 2, 'TALLERES MECANICOS'),
-('2020-04-28 10:25:47', '2020-04-28 10:25:47', 3, 'LAVADO DE AUTOS'),
-('2020-04-28 10:26:00', '2020-04-28 10:26:00', 5, 'TALLERES DE HOJALATERIA Y PINTURA'),
-('2020-04-28 10:26:08', '2020-04-28 10:26:08', 7, 'TALACHERAS'),
-('2020-04-28 10:26:11', '2020-04-28 10:26:11', 8, 'REPARACION DE CAJAS AUTOMATICAS'),
-('2020-05-01 02:20:24', '2020-05-01 02:20:24', 9, 'NEGOCIOS DE AUTO PARTES USADAS'),
-('2020-05-01 02:20:45', '2020-05-01 02:20:45', 10, 'TALLERES ELECTRICOS'),
-('2020-05-01 02:21:34', '2020-05-01 02:21:34', 11, 'REFACCIONARIAS');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `tipos_negocios`
---
-
-CREATE TABLE `tipos_negocios` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `negocio_id` bigint(20) UNSIGNED NOT NULL,
-  `tipo_id` bigint(20) UNSIGNED NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Volcado de datos para la tabla `tipos_negocios`
---
-
-INSERT INTO `tipos_negocios` (`id`, `negocio_id`, `tipo_id`, `created_at`, `updated_at`) VALUES
-(2, 1, 2, '2020-04-28 05:27:02', '2020-04-28 05:27:02'),
-(3, 1, 5, '2020-04-28 05:27:02', '2020-04-28 05:27:02'),
-(10, 2, 5, '2020-04-30 21:14:16', '2020-04-30 21:14:16'),
-(11, 2, 8, '2020-04-30 21:14:16', '2020-04-30 21:14:16'),
-(12, 3, 11, '2020-04-30 21:23:57', '2020-04-30 21:23:57'),
-(25, 4, 2, '2020-04-30 21:44:56', '2020-04-30 21:44:56'),
-(26, 4, 8, '2020-04-30 21:44:56', '2020-04-30 21:44:56'),
-(27, 5, 9, '2020-04-30 21:47:22', '2020-04-30 21:47:22'),
-(28, 6, 11, '2020-04-30 21:51:41', '2020-04-30 21:51:41');
+INSERT INTO `posts` (`id`, `img`, `content`, `created_at`, `updated_at`, `user_id`) VALUES
+(19, '/images/1588607926.jpeg', 'asd', '2020-05-04 20:58:46', '2020-05-04 20:58:46', 1),
+(21, NULL, 'asdjlsa dasdjlas jdlkas', '2020-05-16 19:09:54', '2020-05-16 19:09:54', 6),
+(22, NULL, 'asdsadasdasd', '2020-05-16 19:10:01', '2020-05-16 19:10:01', 6),
+(23, NULL, 'asdasdasdasd', '2020-05-16 19:10:08', '2020-05-16 19:10:08', 6),
+(24, NULL, 'asdasdasdasd', '2020-05-16 19:10:13', '2020-05-16 19:10:13', 6),
+(25, '/images/1589667744.png', 'asdasd', '2020-05-17 03:22:24', '2020-05-17 03:22:24', 7);
 
 -- --------------------------------------------------------
 
@@ -330,29 +257,15 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `address`, `phone`, `remember_token`, `created_at`, `updated_at`, `api_token`, `rol`) VALUES
 (1, 'jovanny ramírez chimal', 'jovannyrch@gmail.com', NULL, '$2y$10$Mf3cWSKrXLP0FjpYqMPn8OtgvnwAEmDxp3negDDQ3rJHcwunAm5M.', 'Toluca Estado de México', '7226227577', NULL, '2020-04-09 01:32:32', '2020-04-09 01:32:32', NULL, 'admin'),
 (2, 'Otro usuario', 'mrgbep@gmail.com', NULL, '$2y$10$f8rligpiI.vdBYy39k6afu94NE3CcV.0VBz5FME44jKvfnp3.wKAy', 'Temascalcingo', '7122080680', NULL, '2020-04-12 09:44:56', '2020-04-28 10:17:15', NULL, 'owner'),
-(3, 'Daniela', 'j19_r10@hotmail.com', NULL, '$2y$10$7WacnlWfxrc2c6pQ0S0C/ueD.971tJ51IacohJiY28Mq8OamAKfhK', 'San Francisco Tepeolulco, Edomex', '7121397374', NULL, '2020-05-01 03:06:08', '2020-05-01 03:06:08', NULL, 'normal');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `visitas`
---
-
-CREATE TABLE `visitas` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `contador` bigint(20) UNSIGNED DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=koi8u;
+(3, 'Daniela', 'j19_r10@hotmail.com', NULL, '$2y$10$7WacnlWfxrc2c6pQ0S0C/ueD.971tJ51IacohJiY28Mq8OamAKfhK', 'San Francisco Tepeolulco, Edomex', '7121397374', NULL, '2020-05-01 03:06:08', '2020-05-01 03:06:08', NULL, 'normal'),
+(4, 'jovanny ramírez chimal', 'thejovazrch@gmail.com', NULL, '$2y$10$fZohTZxkt68RCLaXCxkRSuQmmrraefmZDh/d770osZ8cVSHEXup/y', 'Toluca, Edomex', 'asd', NULL, '2020-05-04 20:58:11', '2020-05-04 20:58:11', NULL, 'normal'),
+(5, 'Otro usuario', 'correo3@gmail.com', NULL, '$2y$10$lF4tEs0306UV2zBlh11LhemK.wTPKQmb0Q3Yf9wxTqs6JXFFaiw9O', 'Toluca, Edomex', '1231231231', NULL, '2020-05-05 00:34:45', '2020-05-05 00:34:45', NULL, 'normal'),
+(6, 'Otro usuario', 'jovannyrch3@gmail.com', NULL, '$2y$10$YrQnfZxoq4NkmJqFkVSpN.Ln3FnUCVmcg8ZNdR4pBOy5xL9iE2m/6', 'Toluca, Edomex', '123123123', NULL, '2020-05-07 07:38:56', '2020-05-07 07:38:56', NULL, 'normal'),
+(7, 'Dueño de negocio', 'correo4@gmail.com', NULL, '$2y$10$bhrHOeiNR351R6M5ulmACO1.inAFzF4sZHsg/kLyl9mnf9svaLu8y', 'Toluca, Edomex', '7121397374', NULL, '2020-05-17 00:17:01', '2020-05-17 00:17:01', NULL, 'normal');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `ads`
---
-ALTER TABLE `ads`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `negocio_id` (`negocio_id`);
 
 --
 -- Indices de la tabla `cars`
@@ -411,20 +324,6 @@ ALTER TABLE `posts`
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indices de la tabla `tipos`
---
-ALTER TABLE `tipos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `tipos_negocios`
---
-ALTER TABLE `tipos_negocios`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `negocio_id` (`negocio_id`),
-  ADD KEY `tipo_id` (`tipo_id`);
-
---
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
@@ -432,38 +331,26 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `users_api_token_unique` (`api_token`);
 
 --
--- Indices de la tabla `visitas`
---
-ALTER TABLE `visitas`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
-
---
--- AUTO_INCREMENT de la tabla `ads`
---
-ALTER TABLE `ads`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `cars`
 --
 ALTER TABLE `cars`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `comments_car_posts`
 --
 ALTER TABLE `comments_car_posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `failed_jobs`
@@ -475,7 +362,7 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT de la tabla `imgs_cars`
 --
 ALTER TABLE `imgs_cars`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `migrations`
@@ -493,41 +380,17 @@ ALTER TABLE `negocios`
 -- AUTO_INCREMENT de la tabla `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
-
---
--- AUTO_INCREMENT de la tabla `tipos`
---
-ALTER TABLE `tipos`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT de la tabla `tipos_negocios`
---
-ALTER TABLE `tipos_negocios`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de la tabla `visitas`
---
-ALTER TABLE `visitas`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
 --
-
---
--- Filtros para la tabla `ads`
---
-ALTER TABLE `ads`
-  ADD CONSTRAINT `ads_ibfk_1` FOREIGN KEY (`negocio_id`) REFERENCES `negocios` (`id`) ON DELETE CASCADE;
 
 --
 -- Filtros para la tabla `cars`
@@ -566,13 +429,6 @@ ALTER TABLE `negocios`
 --
 ALTER TABLE `posts`
   ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
-
---
--- Filtros para la tabla `tipos_negocios`
---
-ALTER TABLE `tipos_negocios`
-  ADD CONSTRAINT `tipos_negocios_ibfk_1` FOREIGN KEY (`negocio_id`) REFERENCES `negocios` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `tipos_negocios_ibfk_2` FOREIGN KEY (`tipo_id`) REFERENCES `tipos` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
